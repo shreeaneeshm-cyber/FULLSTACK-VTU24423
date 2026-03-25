b3e72cbb9b29f1a5126e1fc14db05b4325c0c620
shreeaneeshm-cyber <shreeaneesh.m@gmail.com>
2026-03-25 09:40:58 +0530

    Add files via upload

---
+++ Custom Query using Spring Data JPA/src/main/java/edu/jfs/app/controller/ProductController.java
@@ -0,0 +1,86 @@
+package edu.jfs.app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.jfs.app.entity.Product;
+import edu.jfs.app.service.ProductService;
+
+@RestController
+public class ProductController {
+
+	@Autowired
+	ProductService service;
+
+	@GetMapping("/getProducts")
+	public List<Product> getProducts() {
+
+		return service.getProducts();
+	}
+
+	@GetMapping("/getProductByCost/{cost}")
+	public Product getProductByCost(@PathVariable Double cost) {
+
+		return service.getProductByCost(cost);
+	}
+
+	// mapping to pick only one product by id
+
+	@GetMapping("/getProduct/{id}")
+	public Product getProduct(@PathVariable int id) {
+
+		return service.getProduct(id);
+	}
+
+	// mapping to pick only one product by name
+	@GetMapping("/getProductByName/{name}")
+	public Product getProductByName(@PathVariable String name) {
+
+		return service.getProductByName(name);
+	}
+
+	// post one object to a db(save) using requestbody
+	@PostMapping("/addProduct")
+	public Product addData(@RequestBody Product product) {
+		service.addData(product);
+		return product;
+	}
+	
+	@PutMapping("/updateProduct")
+	public Product updateProduct(@RequestBody Product product) {
+	    service.addData(product);
+	    return product;
+	}
+	
+	@DeleteMapping("/deleteProduct/{id}")
+	public String deleteProduct(@PathVariable int id) {
+	    service.deleteProduct(id);
+	    return "Product Deleted";
+	}
+	
+	@PostMapping("/saveProduct")
+	public ResponseEntity<Product> addDatap(@RequestBody Product product) {
+		service.addData(product);
+		return ResponseEntity.ok(product);
+	}
+	
+	@GetMapping("/getCostGreaterThan/{cost}")
+	public List<Product> getCostGreaterThan(@PathVariable double cost){
+	    return service.getProductsGreaterThan(cost);
+	}
+
+	@GetMapping("/searchProduct/{name}")
+	public List<Product> searchProduct(@PathVariable String name){
+	    return service.getProductsByNameContains(name);
+	}
+
+}
